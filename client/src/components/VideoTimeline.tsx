3cb6e3dbb009ad96f58e2935265b9ea41303745c
kev-damian <maheu@outlook.com>
2023-08-21 21:46:05 +0900

    edit timeline (color, width)

--- client/src/components/VideoTimeline.tsx
+++ client/src/components/VideoTimeline.tsx
@@ -6,7 +6,7 @@ export default function VideoTimeline() {
   const { readRemoteFile } = usePapaParse();
   const [csvData, setCSVData] = useState([]);
 
-  const csvFilePath = "/X_fv_0804_MX_0001.csv";
+  const csvFilePath = "/X_fv_0701_MX_0001.csv";
 
   const handleRemoteFile = () => {
     readRemoteFile(csvFilePath, {
@@ -29,9 +29,9 @@ export default function VideoTimeline() {
     // console.log(timeString)
     if (timeString){
       const timeArray = timeString.split(":");
-      const minute = parseInt(timeArray[1]);
-      const second = parseInt(timeArray[2].split(".")[0]);
-      const frame = parseInt(timeArray[2].split(".")[1]);
+      const minute = parseInt(timeArray[0]);
+      const second = parseInt(timeArray[1].split(".")[0]);
+      const frame = parseInt(timeArray[1].split(".")[1]);
   
       const milliseconds =
         minute * 60 * 1000 +
@@ -44,14 +44,27 @@ export default function VideoTimeline() {
   }
 
   const colormap = (label: string) => {
-  }
+    if (label === "BG") return "#BAB0AC"
+    let label_alphabet = label[0]
+    switch (label_alphabet) {
+      case "M":
+        return "#4E79A7"
+      case "G":
+        return "#F28E2B"
+      case "P":
+        return "#E15759"
+      case "R":
+        return "#76B7B2"
+      case "A":
+        return "#59A14F"
+  }}
 
 
   return (
     <>
-      <div className="flex flex-row gap-1 overflow-auto">
+      <div className="flex flex-row gap-2 overflow-auto">
         {csvData.map((row: any, rowIndex: number) => (
-          <div key={rowIndex} className={`rounded py-1 px-3 border`} style={{width : timeToMilliseconds(row['duration'])}} >
+          <div key={rowIndex} className={`rounded py-2 px-4 border flex-none`} style={{width : timeToMilliseconds(row['duration'])/5, backgroundColor: colormap(row['label'])}} >
             {row["label"]}
           </div>
         ))}
