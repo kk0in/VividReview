48e02ee40ae1a84cf9ad1ede6b15b3cb82b89a26
kev-damian <maheu@outlook.com>
2023-08-23 00:46:51 +0900

    edit app bar navigation <a> to <Link>

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -3,11 +3,12 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 
 const navigation = [
-  { name: 'Home', href: '/', current: false },
-  { name: 'Dashboard', href: '/dashboard', current: true }
+  { name: 'Home', href: '/'},
+  { name: 'Dashboard', href: '/dashboard'}
 ]
 
 
@@ -15,8 +16,13 @@ function classNames(...classes: String[]) {
   return classes.filter(Boolean).join(' ')
 }
 
+function isCurrentPage(href: string) {
+  return window.location.pathname === href
+}
+
 
 export default function AppBar() {
+  
   return (
     <nav className="bg-gray-800">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
@@ -31,17 +37,17 @@ export default function AppBar() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className={classNames(
-                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      isCurrentPage(item.href) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                       'rounded-md px-3 py-2 text-sm font-medium'
                     )}
                     aria-current={item.current ? 'page' : undefined}
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
             </div>
