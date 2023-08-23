51ad1580d6513192545ea5df582d580fd8f5d7be
spark-damian <maheu@outlook.com>
2023-08-23 20:35:33 +0900

    edit server

--- server/server.py
+++ server/server.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI, UploadFile, Form, File
+from fastapi.middleware.cors import CORSMiddleware
+
 import shutil
 import json
 import os
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 os.makedirs('videos', exist_ok=True)
 os.makedirs('metadata', exist_ok=True)
 
-@app.post('/api/uploadfile/')
+@app.post('/api/uploadfile', status_code=201)
 async def upload_file(gbm: str = Form(...), product: str = Form(...), plant: str = Form(...), route: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
     metadata = {  
         'gbm': gbm,  
@@ -27,5 +41,6 @@ async def upload_file(gbm: str = Form(...), product: str = Form(...), plant: str
     metadata_file_path = f"metadata/{video_filename}.json"
     with open(metadata_file_path, 'w') as f:
         json.dump(metadata, f)
+
 
     return {"video_file": video_file_path, "metadata_file": metadata_file_path}
