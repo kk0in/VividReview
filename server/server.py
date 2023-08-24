f1badfabf63aa4237e366a934ce31651f945669c
spark-damian <maheu@outlook.com>
2023-08-24 22:21:53 +0900

    add: load project / post project / get ~ / option result

--- server/server.py
+++ server/server.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, UploadFile, Form, File
+from fastapi import FastAPI, UploadFile, Form, File, HTTPException, Body
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
+
+from typing import Any
 
 import zipfile
 import shutil
@@ -21,31 +23,154 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-os.makedirs('videos', exist_ok=True)
-os.makedirs('metadata', exist_ok=True)
+VIDEO = './videos'
+KEYPOINT = './keypoints'
+FEATURE_VECTOR = './feature_vectors'
+RESULT = './results'
+META_DATA = './metadata'
 
-@app.post('/api/uploadfile', status_code=201)
-async def upload_file(gbm: str = Form(...), product: str = Form(...), plant: str = Form(...), route: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+os.makedirs(VIDEO, exist_ok=True)
+os.makedirs(META_DATA, exist_ok=True)
+os.makedirs(KEYPOINT, exist_ok=True)
+os.makedirs(FEATURE_VECTOR, exist_ok=True)
+os.makedirs(RESULT, exist_ok=True)
+
+
+def issue_id():
+    metadata_list = [file for file in os.listdir(META_DATA) if file.endswith('.json')]
+    if metadata_list:
+        ids = [int(file.split('_')[0]) for file in metadata_list]
+        return max(ids) + 1
+    
+    else:
+        return 1
+    
+def issue_version(project_id):
+    result_list = [file for file in os.listdir(RESULT) if file.startswith(f'{project_id}_') and file.endswith('.json')]
+    if result_list:
+        versions = [int(file.split('_')[-1].split('.')[0]) for file in result_list]
+        return max(versions) + 1
+    else:
+        return 1
+    
+def get_filename(id):
+    metadata_list = [file for file in os.listdir(META_DATA) if file.endswith('.json')]
+    if metadata_list:
+        for file in metadata_list:
+            if file.startswith(f'{id}_'):
+                # if file name is 1_0707_MX_0002_TEST.json, return 0707_MX_0002_TEST
+                fn = os.path.basename(file).split('.')[0].split('_')[1:]
+                fn = '_'.join(fn)
+                return fn
+    else:
+        return None
+
+@app.get('/api/get_project', status_code=200)
+async def get_project():
+    metadata_list = [file for file in os.listdir(META_DATA) if file.endswith('.json')]
+    project_list = []
+
+    for metadata in metadata_list:
+        with open(os.path.join(META_DATA, metadata), 'r') as f:
+            project_list.append(json.load(f))
+
+    return {"projects": project_list}
+
+@app.get('/api/get_project/{project_id}', status_code=200)
+async def get_project(project_id: int):
+    metadata_file = [file for file in os.listdir(META_DATA) if file.startswith(f'{project_id}_') and file.endswith('.json')]
+
+    if not metadata_file:
+        raise HTTPException(status_code=404, detail="Project not found")
+    
+    if len(metadata_file) > 1:
+        raise HTTPException(status_code=500, detail="Multiple projects found")
+    else:
+        with open(os.path.join(META_DATA, metadata_file[0]), 'r') as f:
+            return {"project": json.load(f)}
+        
+@app.get('/api/get_video/{project_id}', status_code=200)
+async def get_video(project_id: int):
+    video_file = [file for file in os.listdir(VIDEO) if file.startswith(f'{project_id}_') and file.endswith('.mp4')]
+
+    if not video_file:
+        raise HTTPException(status_code=404, detail="Video not found")
+    
+    if len(video_file) > 1:
+        raise HTTPException(status_code=500, detail="Multiple videos found")
+    else:
+        return FileResponse(os.path.join(VIDEO, video_file[0]), media_type='video/mp4')
+    
+@app.get('/api/get_keypoint/{project_id}', status_code=200)
+async def get_keypoint(project_id: int):
+    keypoint_file = [file for file in os.listdir(KEYPOINT) if file.startswith(f'{project_id}_') and file.endswith('.json')]
+
+    if not keypoint_file:
+        raise HTTPException(status_code=404, detail="Keypoint not found")
+    
+    if len(keypoint_file) > 1:
+        raise HTTPException(status_code=500, detail="Multiple keypoints found")
+    else:
+        with open(os.path.join(KEYPOINT, keypoint_file[0]), 'r') as f:
+            return {"keypoint": json.load(f)}
+        
+@app.get('/api/get_result/{project_id}', status_code=200)
+async def get_result(project_id: int):
+    result_file = [file for file in os.listdir(RESULT) if file.startswith(f'{project_id}_') and file.endswith('.json')]
+
+    if not result_file:
+        raise HTTPException(status_code=404, detail="Result not found")
+    
+    if len(result_file) > 1:
+        result_file = sorted(result_file, key=lambda x: int(x.split('_')[-1].split('.')[0]))
+        with open(os.path.join(RESULT, result_file[-1]), 'r') as f:
+            return {"result": json.load(f)}
+    else:
+        with open(os.path.join(RESULT, result_file[0]), 'r') as f:
+            return {"result": json.load(f)}
+        
+@app.options('/api/update_result/{project_id}', status_code=200)
+async def update_result(project_id: int, result: Any = Body(...)):
+    version = issue_version(project_id)
+    file_name = get_filename(project_id)
+
+    result_file_path = f"{RESULT}/{project_id}_{file_name}_{version}.json"
+
+    with open(result_file_path, 'w') as f:
+        json.dump(result, f)
+
+    return {"id": project_id, "version": version}
+    
+        
+@app.post('/api/upload_project', status_code=201)
+async def upload_project(gbm: str = Form(...), product: str = Form(...), plant: str = Form(...), route: str = Form(...), description: str = Form(...), file: UploadFile = File(...)):
+    id = issue_id()
+
     metadata = {  
+        'id': id,
         'gbm': gbm,  
         'product': product,  
         'plant': plant,  
         'route': route,  
-        'description': description,  
+        'description': description,
+        'done': 0
     }
 
     video_filename = os.path.splitext(file.filename)[0]
 
-    video_file_path = f"videos/{file.filename}"
+    video_file_path = f"{VIDEO}/{id}_{file.filename}"
     with open(video_file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    metadata_file_path = f"metadata/{video_filename}.json"
+    metadata_file_path = f"{META_DATA}/{id}_{video_filename}.json"
     with open(metadata_file_path, 'w') as f:
         json.dump(metadata, f)
 
+    return {"id": id}
 
-    return {"video_file": video_file_path, "metadata_file": metadata_file_path}
+
+######
+
 
 @app.post('/test/upload_video/')
 async def upload_file(file: UploadFile = File(...)):
