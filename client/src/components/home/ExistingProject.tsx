4dff8884deef4d76b3e4002796949575451af9cc
songbird39 <sjw_512@naver.com>
2023-08-31 17:38:59 +0900

    format Existing Projects

--- client/src/components/home/ExistingProject.tsx
+++ client/src/components/home/ExistingProject.tsx
@@ -26,41 +26,51 @@ const ExistingProject: React.FC<ExistingrojectProps> = ({ setView }) => {
         </h4>
       </div>
       <div>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>GBM</th>
-              <th>Product</th>
-              <th>Plant</th>
-              <th>Route</th>
-              <th>Description</th>
-              <th>Done</th>
-            </tr>
-          </thead>
-          <tbody>
-            {projectListData &&
-              projectListData.projects.map((project: any) => (
-                <tr key={project.id}>
-                  <td>{project.id}</td>
-                  <td>{project.gbm}</td>
-                  <td>{project.product}</td>
-                  <td>{project.plant}</td>
-                  <td>{project.route}</td>
-                  <td>{project.description}</td>
-                  <td>
-                    {project.done ? (
-                      <Link href={`/dashboard/${project.id}`}>
-                        Go to dashboard
-                      </Link>
-                    ) : (
-                      "NOT DONE"
-                    )}
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
+        <div className="mt-4 mb-3">
+          <div className="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
+            <div style={{ backgroundPosition: '10px 10px' }} className="absolute inset-0 bg-grid-slate-100 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.6))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"></div>
+              <div className="relative rounded-xl overflow-auto">
+                <div className="shadow-sm overflow-hidden my-8">
+                  <table className="border-collapse table-auto w-full text-sm">
+                    <thead>
+                      <tr>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">ID</th>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">GBM</th>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Product</th>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Plant</th>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Route</th>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Description</th>
+                        <th className="border-b dark:border-slate-600 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">Done</th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-white dark:bg-slate-800">
+                      {projectListData &&
+                        projectListData.projects.map((project: any) => (
+                          <tr key={project.id}>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{project.id}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{project.gbm}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{project.product}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{project.plant}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{project.route}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">{project.description}</td>
+                            <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                              {project.done ? (
+                                <Link href={`/dashboard/${project.id}`}>
+                                  Go to dashboard
+                                </Link>
+                              ) : (
+                                "NOT DONE"
+                              )}
+                            </td>
+                          </tr>
+                        ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            <div className="absolute inset-0 pointer-events-none border border-black/5 rounded-xl dark:border-white/5"></div>
+          </div>
+        </div>
       </div>
       <button onClick={() => setView("select")}>go back</button>
     </div>
