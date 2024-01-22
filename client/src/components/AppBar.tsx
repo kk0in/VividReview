e9574aba2bf0153f42d4672406bcb04dd256014f
spark-damian <maheu@outlook.com>
2024-01-22 16:06:32 +0900

    app bar redundant

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -11,7 +11,6 @@ import { usePathname } from 'next/navigation'
 const navigation = [
   { name: 'Home', href: '/'},
   { name: 'Projects', href: '/projects'},
-  // { name: 'Add New Project', href: '/new'}
 ]
 
 
@@ -29,7 +28,7 @@ export default function AppBar() {
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
+            {/* Mobile menu button here*/}
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center text-white font-bold">
