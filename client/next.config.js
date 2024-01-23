912eba312dc4c5425c57db6e81887176a3b34a18
spark-damian <maheu@outlook.com>
2024-01-23 13:33:34 +0900

    update server env in nextconfig

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        SERVER_ENDPOINT: process.env.SERVER_ENDPOINT,
+    }
+}
 
 module.exports = nextConfig
