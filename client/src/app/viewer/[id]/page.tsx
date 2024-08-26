d902cdcd7b117811f8c76b454c65b2454106df3b
ZzeongB <>
2024-08-26 12:52:05 +0900

    remove round edge

--- client/src/app/viewer/[id]/page.tsx
+++ client/src/app/viewer/[id]/page.tsx
@@ -1689,7 +1689,7 @@ export default function Page({ params }: { params: { id: string } }) {
               )}
             </SearchModal>
             {isReviewMode && (
-              <div className="flex flex-col rounded-2xl bg-gray-200" ref={containerRef}>
+              <div className="flex flex-col bg-gray-200" ref={containerRef}>
                 <ArousalGraph
                   data={prosodyData}
                   handleAudioRef={handleAudioRef}
