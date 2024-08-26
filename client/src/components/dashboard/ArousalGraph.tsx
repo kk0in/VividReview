5d760e53253775f87472fd3614be1000bee37885
ZzeongB <>
2024-08-26 15:11:57 +0900

    cleanup code for arousal graph

--- client/src/components/dashboard/ArousalGraph.tsx
+++ client/src/components/dashboard/ArousalGraph.tsx
@@ -13,6 +13,8 @@ import {
 } from "recharts";
 import { gridModeState } from "@/app/recoil/ToolState";
 import { CategoricalChartFunc } from "recharts/types/chart/generateCategoricalChart";
+import { CustomRectangle, CustomLegend, CurrentPositionLine, CustomToggleSwitch, CustomXAxisTick, HorizontalLine } from "./GraphComponent"
+import { CategoricalChartState } from "recharts/types/chart/types";
 
 const GRAPH_HEIGHT = 200;
 const X_AXIS_HEIGHT = 20;
@@ -79,24 +81,6 @@ const processTableOfContents = (tableOfContents: any) => {
   return pageToTitleSubtitleMap;
 };
 
-const CustomRectangle = ({
-  pageStartTime,
-  pageEndTime,
-}: {
-  pageStartTime: number;
-  pageEndTime: number;
-}) => {
-  return (
-    <Rectangle
-      x={pageStartTime}
-      y={0}
-      width={pageEndTime - pageStartTime}
-      height={GRAPH_HEIGHT - X_AXIS_HEIGHT} // Use 100 to fill the entire height
-      fill="rgba(0,0,0,0.3)"
-    />
-  );
-};
-
 const calculateStartAndEnd = (
   pageKey: number | string,
   gridMode: number,
@@ -120,139 +104,6 @@ const calculateStartAndEnd = (
       break;
   }
   return { start, end };
-};
-
-const CustomXAxisTick = ({
-  x,
-  y,
-  payload,
-  currentXTick,
-  tableOfContentsMap,
-  graphWidth,
-  findPage,
-  images,
-}: {
-  x: number;
-  y: number;
-  payload: any;
-  currentXTick: number;
-  tableOfContentsMap: any;
-  graphWidth: number;
-  findPage: any;
-  images: any;
-}) => {
-  const titleSubtitle = tableOfContentsMap[currentXTick];
-  const pageNumber = findPage(payload.value);
-
-  if (titleSubtitle && pageNumber === currentXTick) {
-    const { subtitle } = titleSubtitle;
-    const text = `${subtitle}`;
-
-    const willTextOverflowRight = x + subtitle.length * 7 > graphWidth;
-    let textAnchor = willTextOverflowRight ? "end" : "start";
-
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <image
-          x={textAnchor == "end"? -80: 0}
-          y={-100}
-          width={100}
-          height={100}
-          href={images[currentXTick - 1]} // `href` 속성을 사용하여 이미지 삽입
-          opacity={0.5}
-
-        />
-        <text
-          x={0}
-          y={0}
-          dy={7}
-          textAnchor={textAnchor}
-          fill={"#666"}
-          fontSize={12}
-        >
-          {text}
-        </text>
-      </g>
-    );
-  }
-
-  return null;
-};
-
-const CurrentPositionLine = ({ x }: { x: number }) => (
-  <line
-    x1={x}
-    y1={0}
-    x2={x}
-    y2={GRAPH_HEIGHT - X_AXIS_HEIGHT}
-    stroke="#008014"
-    strokeWidth={3} // Reduced thickness
-    opacity={0.5} // Added opacity for transparency
-  />
-);
-
-const HorizontalLine = ({
-  x1,
-  x2,
-  color,
-  y,
-}: {
-  x1: number;
-  x2: number;
-  color: string;
-  y: number;
-}) => {
-  return <line x1={x1} y1={y} x2={x2} y2={y} stroke={color} strokeWidth={3} />;
-};
-
-const CustomLegend = (props) => {
-  const FONT_COLOR = "#333";
-  const FONT_SIZE = "10px";
-  const { payload } = props;
-  return (
-    <div
-      style={{ display: "flex", justifyContent: "center", marginBottom: 10 }}
-    >
-      {payload.map((entry, index) => (
-        <div
-          key={`item-${index}`}
-          style={{ display: "flex", alignItems: "center", marginRight: 20 }}
-        >
-          <div
-            style={{
-              width: 20,
-              height: 3,
-              backgroundColor: entry.color,
-              marginRight: 5,
-            }}
-          />
-          <span style={{ color: FONT_COLOR, fontSize: FONT_SIZE }}>{entry.value}</span>
-        </div>
-      ))}
-      <div style={{ display: "flex", alignItems: "center", marginRight: 20 }}>
-        <div
-          style={{
-            width: 20,
-            height: 3,
-            backgroundColor: "red",
-            marginRight: 5,
-          }}
-        />
-        <span style={{ color: FONT_COLOR, fontSize: FONT_SIZE }}>Missed Part</span>
-      </div>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <div
-          style={{
-            width: 20,
-            height: 3,
-            backgroundColor: "green",
-            marginRight: 5,
-          }}
-        />
-        <span style={{ color: FONT_COLOR, fontSize: FONT_SIZE }}>Important Part</span>
-      </div>
-    </div>
-  );
 };
 
 const useProcessedData = (
@@ -298,124 +149,6 @@ const useMinMaxValues = (processedData: any[]) => {
   );
 
   return { minY, maxY, minX, maxX };
-};
-
-const ToggleSwitch = ({
-  label,
-  checked,
-  onChange,
-  color,
-}: {
-  label: string;
-  checked: boolean;
-  onChange: () => void;
-  color: string;
-}) => {
-  return (
-    <label style={{ display: "flex", alignItems: "center", marginRight: 10 }}>
-      <div style={{ position: "relative", display: "inline-block", width: "24px", height: "12px" }}>
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={onChange}
-          style={{
-            opacity: 0,
-            width: 0,
-            height: 0,
-          }}
-        />
-        <span
-          style={{
-            position: "absolute",
-            cursor: "pointer",
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: checked ? color : "#D8D6F9",  // Updated colors
-            transition: ".4s",
-            borderRadius: "12px",
-          }}
-        />
-        <span
-          style={{
-            position: "absolute",
-            content: "",
-            height: "8px",
-            width: "8px",
-            borderRadius: "50%",
-            backgroundColor: "white",
-            transition: ".4s",
-            transform: checked ? "translateX(12px)" : "translateX(0)",
-            top: "2px",
-            left: "2px",
-          }}
-        />
-      </div>
-      <span style={{ marginLeft: 8, color: "#333", fontSize: "12px" }}>
-        {label}
-      </span>
-    </label>
-  );
-};
-
-const CustomToggleSwitch = ({
-  positiveEmotion,
-  negativeEmotion,
-  selectedPositives,
-  selectedNegatives,
-  handlePositiveToggle,
-  handleNegativeToggle,
-}: {
-  positiveEmotion: string[];
-  negativeEmotion: string[];
-  selectedPositives: boolean[];
-  selectedNegatives: boolean[];
-  handlePositiveToggle: (index: number) => void;
-  handleNegativeToggle: (index: number) => void;
-}) => {
-  return (
-    <div>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: 20,
-        marginTop: 15,
-        paddingTop: 10,
-        backgroundColor: "#E5E7EB",
-      }}
-    >
-      {positiveEmotion.map((label, index) => (
-        <ToggleSwitch
-          key={`positive-${index}`}
-          label={label}
-          checked={selectedPositives[index]}
-          onChange={() => handlePositiveToggle(index)}
-          color = "#8884d8"
-        />
-      ))}
-    </div>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: 20,
-        backgroundColor: "#E5E7EB",
-      }}
-    >
-      {negativeEmotion.map((label, index) => (
-        <ToggleSwitch
-          key={`negative-${index}`}
-          label={label}
-          checked={selectedNegatives[index]}
-          onChange={() => handleNegativeToggle(index)}
-          color = "#82ca9d"
-        />
-      ))}
-    </div>
-    </div>
-  );
 };
 
 const ArousalGraph = ({
@@ -660,7 +393,6 @@ const ArousalGraph = ({
           />
         )}
         {missedAndImportantParts?.missed.map((part: any) => {
-          // horizontal line
           return (
             <Customized
               component={
