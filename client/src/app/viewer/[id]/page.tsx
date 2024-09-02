dc2f79c8f7190ef7bd1d6b9ec34e7b98215a8407
kk0in <kyi8871@naver.com>
2024-09-02 14:54:49 +0900

    Fix mb from 12 to 8

--- client/src/app/viewer/[id]/page.tsx
+++ client/src/app/viewer/[id]/page.tsx
@@ -1719,7 +1719,7 @@ export default function Page({ params }: { params: { id: string } }) {
             </SearchModal>
             {isReviewMode && (
               <div
-                className="bg-white rounded-lg shadow-xl shadow-slate-200 h-fit mb-12 xl:mb-0"
+                className="bg-white rounded-lg shadow-xl shadow-slate-200 h-fit mb-8"
                 ref={containerRef}
               >
                 <ArousalGraph
